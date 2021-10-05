778f2275e1607ac0b258cf0e0e7bf9e0092f8f36
balayokesh <balayokeshmani@gmail.com>
2021-10-05 17:31:31 +0530

    Added Dashboard page content

--- react-template/src/Dashboard.jsx
+++ react-template/src/Dashboard.jsx
@@ -1,15 +1,45 @@
 import { Link } from "react-router-dom";
 
 // Import MaterialUI components
-import { Container, Typography, Button } from "@material-ui/core";
+import { makeStyles, Container, AppBar, Typography, Card, CardContent } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
+
+const useStyles = makeStyles ({
+    flexy: {
+        display: 'flex',
+    },
+    cardContainer: {
+        width: '45vw',
+        height: '45vh',
+        display: 'inline-block',
+    }
+});
 
 export default function Dashboard () {
+    const classes = useStyles();
     return (
         <Container>
-            <Button>
-                <Link to='/'>Home</Link>
-            </Button>
-            <Typography variant='h3'>Dashboard</Typography>
+            <AppBar>
+                <Typography variant='h3' align='center'>Dashboard</Typography>
+            </AppBar>
+            <br />            
+            <br />
+            <br />
+            <Link to='/'>Home</Link>
+            <div className='classes.flexy'>
+                <Card className={classes.cardContainer}>
+                    <CardContent>
+                        <Typography variant='h5' align='center'>Activities</Typography>
+                        <img src='https://source.unsplash.com/random' alt='random image' />
+                    </CardContent>
+                </Card>
+                <Card className={classes.cardContainer}>
+                    <CardContent>
+                        <Typography variant='h5' align='center'>Demographics</Typography>
+                        <img src='https://source.unsplash.com/random' alt='random image' />
+                    </CardContent>
+                </Card>
+            </div>
         </Container>
     )
 }
