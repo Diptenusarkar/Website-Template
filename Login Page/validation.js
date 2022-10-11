662f18719b4cbb4bf08a7e73f12e51bb7f2c931a
Sumedh Phade <smpsumedh13@gmail.com>
2022-10-11 14:10:59 +0530

    Updated solution for issue #52

--- Login Page/validation.js
+++ Login Page/validation.js
@@ -2,7 +2,7 @@ function check(event){
     event.preventDefault();
     var email1=document.getElementById('mail');
     var passd1=document.getElementById('pswd');
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var mailformat = /\S+@\S+\.\S+/;
     var passformat=/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
     
         if(email1.value.match(mailformat) && passd1.value.match(passformat)){
