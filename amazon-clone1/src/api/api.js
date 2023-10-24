865f6463f85bfa493b8903b646cd3562248a75dd
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2023-10-24 22:47:51 +0530

    delete extra files

--- amazon-clone1/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function productsData(){
-    // const products = await axios.get("https://fakestoreapiserver.vercel.app/amazonproducts");
-    const products = await axios.get("https://fakestoreapi.com/products");
-
-    return products
-    
-}
--- amazon-clone1/src/constants/roundedPrice.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RoundedPrice = ({ price }) => {
-  return (
-    <p className='text-sm text-gray-600 font-semibold'>₹{Math.ceil(price*80)}</p>
-  );
-};
-
-export default RoundedPrice;
